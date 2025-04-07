788dfa968fefb3bfb02343a0ff22ad152b0ef8b9
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-07 19:54:18 +0530

    updated the repo

--- DemowebShop/src/test/java/runnerpack/WebRunner.java
+++ DemowebShop/src/test/java/runnerpack/WebRunner.java
@@ -1,50 +1,25 @@
-//package runnerpack;
-//
-//import org.testng.annotations.Listeners;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-//
-//@Listeners(utilities.Listenerclass.class)
-//@CucumberOptions(
-//    features = {
-//        "src/test/resources/feature/login.feature",
-//        "src/test/resources/feature/Newsletter.feature",
-//        "src/test/resources/feature/cart.feature"
-//    },
-//    glue = "definition",
-//    plugin = {
-//        "pretty",
-//        "html:target/cucumber-reports.html",
-//        "json:target/cucumber-reports.json",
-//        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-//    }
-//)
-//public class WebRunner extends AbstractTestNGCucumberTests {
-//}
-
 package runnerpack;
 
 import org.testng.annotations.Listeners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-// Optional: Attach your custom TestNG listener (for logs/screenshots/etc)
 @Listeners(utilities.Listenerclass.class)
-
 @CucumberOptions(
     features = {
         "src/test/resources/feature/login.feature",
         "src/test/resources/feature/Newsletter.feature",
         "src/test/resources/feature/cart.feature"
     },
-    glue = "definition",  // Package where your step defs are
+    glue = "definition",
     plugin = {
         "pretty",
-        "html:target/cucumber-reports.html",                            // Basic HTML report
-        "json:target/cucumber-reports.json",                            // Required for Masterthought report
-        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" // ExtentReports
-    }          // Optional: disable Cucumber Cloud publish
+        "html:target/cucumber-reports.html",                              // Simple HTML
+        "json:target/cucumber-reports.json",                              // Required for Masterthought
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" ,
+        "html:target/overview-features.html"
+// ExtentReports
+    }
 )
 public class WebRunner extends AbstractTestNGCucumberTests {
 }
-
