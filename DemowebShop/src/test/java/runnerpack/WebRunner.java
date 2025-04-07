855b08ac831df0897b2184a9cb4639208af3f47e
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-07 16:32:37 +0530

    change the json path file

--- DemowebShop/src/test/java/runnerpack/WebRunner.java
+++ DemowebShop/src/test/java/runnerpack/WebRunner.java
@@ -1,18 +1,22 @@
 package runnerpack;
 
 import org.testng.annotations.Listeners;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 @Listeners(utilities.Listenerclass.class)
 @CucumberOptions(
-    features = {"src/test/resources/feature/login.feature","src/test/resources/feature/Newsletter.feature","src/test/resources/feature/cart.feature"},
+    features = {
+        "src/test/resources/feature/login.feature",
+        "src/test/resources/feature/Newsletter.feature",
+        "src/test/resources/feature/cart.feature"
+    },
     glue = "definition",
     plugin = {
         "pretty",
         "html:target/cucumber-reports.html",
-        "json:target/cucumber-reports.json",
-        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "json:target/cucumber-report/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
     }
 )
 public class WebRunner extends AbstractTestNGCucumberTests {
